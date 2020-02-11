1f8e09f698a595961e255138a8edde99d211b7e9
JulianCRA <contact@julianr.dev>
2020-02-11 18:32:02 +0700

    First commit.

---
+++ src/js/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+
+	render() {
+		return (
+			<React.Fragment>
+				Welcome back, 
+			</React.Fragment>
+		)
+	}
+}
+
+export default App
