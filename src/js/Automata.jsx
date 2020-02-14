6ef4c06444671c66391c41ff104251e5432e28dc
JulianCRA <contact@julianr.dev>
2020-02-14 21:16:48 +0700

    Very rudimentary horizontal scroll.

--- src/js/Automata.jsx
+++ src/js/Automata.jsx
@@ -13,7 +13,9 @@ const Automata = () => {
 	const mql = window.matchMedia('(max-width: 760px)')
 	const [smallViewport, setSmallViewport] = React.useState(mql.matches)
 	
-	console.log('mql :', mql)
+	const [position, setPosition] = React.useState(0)
+
+	console.log('Render Automata')
 	React.useEffect(
 		() => {
 			mql.onchange = () => setSmallViewport(mql.matches)
@@ -27,18 +29,62 @@ const Automata = () => {
 		[]
 	)
 
-	const move = (p) => {
-		console.log("move to ", p)
+	const ac = React.createRef()
+		
+	let pp = 0
+
+	const moveContentTo = (p) => {
+		if(p === pp){
+			console.log("SKETCH")
+			pp = 1
+			// l = document.getElementById('uno').getBoundingClientRect().width
+		}else if(p === 0){
+			console.log("MENU")
+			pp = 0
+			// l = 0
+		}else if(p === 2){
+			console.log("CONFIG")
+			pp = 2
+			// l = document.getElementById('uno').getBoundingClientRect().width + document.getElementById('scrollable').getBoundingClientRect().width
+		}
+		console.log(ac.current);
+		ac.current.scrollTo({
+			left: 400 * pp,
+			top:0,
+			behavior:"smooth"
+		})
 	}
+
+	// const moveContentTo = (p) => {
+	// 	if(p === position){
+	// 		console.log("SKETCH")
+	// 		setPosition(1)
+	// 		// l = document.getElementById('uno').getBoundingClientRect().width
+	// 	}else if(p === 0){
+	// 		console.log("MENU")
+	// 		setPosition(0)
+	// 		// l = 0
+	// 	}else if(p === 2){
+	// 		console.log("CONFIG")
+	// 		setPosition(2)
+	// 		// l = document.getElementById('uno').getBoundingClientRect().width + document.getElementById('scrollable').getBoundingClientRect().width
+	// 	}
+
+	// 	ac.current.scrollTo({
+	// 		left: 200 * p,
+	// 		top:0,
+	// 		behavior:"smooth"
+	// 	})
+	// }
 
 	return (
 		<AutomataProvider>
 			{
 				smallViewport ?
 					<div className = {cx(styles.container, {[styles.small]: smallViewport } ) }>
-						<div className = {styles.menuToggler} onclick="move(0)">MENU</div>
-						<div className = {styles.configToggler} onclick="move(2)">CONFIG</div>
-						<div className = {styles.content} id="content">
+						<div className = {styles.menuToggler} onClick = {() => moveContentTo(0)}>MENU</div>
+						<div className = {styles.configToggler} onClick = {() => moveContentTo(2)}>CONFIG</div>
+						<div className = {styles.content} id = "automata-content"  ref={ac}>
 							<div className = {cx(styles.menuContainer, {[styles.small]: smallViewport } ) }>
 								<Menu small = {smallViewport}/>
 							</div>
